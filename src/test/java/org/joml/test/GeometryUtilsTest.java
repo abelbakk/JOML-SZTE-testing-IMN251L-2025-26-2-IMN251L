08e3c978f074b0eb8270d2211ca1899375f5294c
abelbakk <abelbakk@pm.me>
2026-04-26 11:13:21 +0200

    Remove unnecessary import

--- src/test/java/org/joml/test/GeometryUtilsTest.java
+++ src/test/java/org/joml/test/GeometryUtilsTest.java
@@ -27,7 +27,6 @@ import org.joml.*;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
